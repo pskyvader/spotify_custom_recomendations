c494bb81b1cd8c48471a5395ac3231cd694f026c
Pablo <pskyvader@hotmail.com>
2021-11-02 01:18:49 -0500

    --- Auto-Git Commit ---

--- src/Playlists.jsx
+++ src/Playlists.jsx
@@ -6,54 +6,70 @@ import {
 	ListItemButton,
 	ListItemIcon,
 	ListItemText,
-	Divider,
 } from "@mui/material";
+import Avatar from "@mui/material/Avatar";
 
-import { Inbox, Drafts } from "@mui/icons-material";
-
-import { MePlaylist } from "./API/Me";
+import Me, { MePlaylist } from "./API/Me";
 
 const Playlists = () => {
 	const [playlist, setPlaylist] = useState(<CircularProgress />);
 
-	const [selectedIndex, setSelectedIndex] = useState(0);
+	const ItemsList = ({ items,me }) => {
+		const [selectedIndex, setSelectedIndex] = useState(0);
 
-	const handleListItemClick = (event, index) => {
-		setSelectedIndex(index);
+		const handleListItemClick = (event, index) => {
+			setSelectedIndex(index);
+		};
+
+		let item_list = [];
+		items.forEach((element, key) => {
+            console.log(element);
+            if(me.id===element.owner.id){
+                
+			item_list.push(
+				<ListItemButton
+					key={key}
+					selected={selectedIndex === key}
+					onClick={(event) => handleListItemClick(event, key)}
+				>
+					<ListItemIcon>
+						<Avatar
+							alt={element.name}
+							src={element.images[2].url}
+						/>
+					</ListItemIcon>
+					<ListItemText primary={element.name} />
+				</ListItemButton>
+			);
+        }
+		});
+		return item_list;
+	};
+
+	const Playlists_template = (response,me) => {
+		return (
+			<Box
+				sx={{
+					width: "100%",
+					maxWidth: 360,
+					bgcolor: "background.paper",
+				}}
+			>
+				<List component="nav" aria-label="playlists">
+					<ItemsList items={response.items} me={me}/>
+				</List>
+			</Box>
+		);
 	};
 
 	useEffect(() => {
-		const Playlists_template = (response) => {
-			console.log(response.items);
-
-			return (
-				<Box sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper", }} >
-					<List component="nav" aria-label="playlists">
-						{
-                            response.items.forEach(element => {
-                                console.log(element)
-                                return <ListItemButton
-							selected={selectedIndex === 0}
-							onClick={(event) => handleListItemClick(event, 0)}
-						>
-							<ListItemIcon>
-								<Inbox />
-							</ListItemIcon>
-							<ListItemText primary="Inbox" />
-						</ListItemButton>
-                            })
-                        }
-                        
-
-					</List>
-				</Box>
-			);
-		};
-
 		MePlaylist().then((response) => {
-			setPlaylist(Playlists_template(response));
+            Me().then((me) => {
+                setPlaylist(Playlists_template(response,me));
+            });
+			
 		});
-	}, [selectedIndex]);
+	}, []);
 
 	return playlist;
 };
