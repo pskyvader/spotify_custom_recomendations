0d96b83d784087434a7c282becb7c07237a3032f
Pablo <pskyvader@hotmail.com>
2021-11-04 17:47:06 -0500

    --- Auto-Git Commit ---

--- src/Playlists.jsx
+++ src/Playlists.jsx
@@ -6,6 +6,8 @@ import {
 	ListItemButton,
 	ListItemIcon,
 	ListItemText,
+	Grid,
+	Container,
 } from "@mui/material";
 import Avatar from "@mui/material/Avatar";
 
@@ -14,7 +16,7 @@ import Me, { MePlaylist } from "./API/Me";
 const Playlists = () => {
 	const [playlist, setPlaylist] = useState(<CircularProgress />);
 
-	const ItemsList = ({ items,me }) => {
+	const ItemsList = ({ items, me }) => {
 		const [selectedIndex, setSelectedIndex] = useState(0);
 
 		const handleListItemClick = (event, index) => {
@@ -23,11 +25,11 @@ const Playlists = () => {
 
 		let item_list = [];
 		items.forEach((element, key) => {
-            console.log(element);
-            if(me.id===element.owner.id){
-                
 			item_list.push(
 				<ListItemButton
+					disabled={
+						me.id !== element.owner.id && !element.colaborative
+					}
 					key={key}
 					selected={selectedIndex === key}
 					onClick={(event) => handleListItemClick(event, key)}
@@ -41,33 +43,31 @@ const Playlists = () => {
 					<ListItemText primary={element.name} />
 				</ListItemButton>
 			);
-        }
 		});
 		return item_list;
 	};
 
-	const Playlists_template = (response,me) => {
+	const Playlists_template = (response, me) => {
 		return (
-			<Box
-				sx={{
-					width: "100%",
-					maxWidth: 360,
-					bgcolor: "background.paper",
-				}}
-			>
-				<List component="nav" aria-label="playlists">
-					<ItemsList items={response.items} me={me}/>
-				</List>
-			</Box>
+			<Container>
+				<Grid container spacing={2}>
+					<Grid item xs={4}>
+						<Box>
+							<List component="nav" aria-label="playlists">
+								<ItemsList items={response.items} me={me} />
+							</List>
+						</Box>
+					</Grid>
+				</Grid>
+			</Container>
 		);
 	};
 
 	useEffect(() => {
 		MePlaylist().then((response) => {
-            Me().then((me) => {
-                setPlaylist(Playlists_template(response,me));
-            });
-			
+			Me().then((me) => {
+				setPlaylist(Playlists_template(response, me));
+			});
 		});
 	}, []);
 
