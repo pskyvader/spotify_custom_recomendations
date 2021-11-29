dc6b49b9e4f842c31c62bcd1add090d49a5a8a68
Pablo <pskyvader@hotmail.com>
2021-11-28 23:54:19 -0600

    --- Auto-Git Commit ---

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ app.get("*", (req, res) => {
 
 // if not in production use the port 5000
 const PORT = process.env.PORT || 5000;
-console.log("server started on port:", PORT);
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
