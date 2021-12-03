e96a871eb0d25b9b17f2f5588ab841c4b9c06b14
Pablo <pskyvader@hotmail.com>
2021-12-02 18:30:33 -0600

    --- Auto-Git Commit ---

--- client/src/API/Request.js
+++ client/src/API/Request.js
@@ -1,32 +1,32 @@
 const GetRequest = async (url, method = "GET", body = null,request=null) => {
-	const access_token = localStorage.getItem("access_token");
 	const requestOptions = request || {
 		method: method,
 		headers: {
-			"Content-Type": "application/json",
-			Authorization: "Bearer " + access_token,
+			"Content-Type": "application/json"
 		},
 		body: body,
 	};
-	return fetch(url, requestOptions).then((response) => {
+	const result= async () => {
+		const response = await fetch(url, requestOptions);
 		if (!response.ok) {
-			return response.text().then((responsetext) => {
-				let responsejson = responsetext;
-				try {
-					responsejson = JSON.parse(responsetext);
-				} finally {
-					return {
-						error: true,
-						status: response.status,
-						text: responsetext,
-						url: response.url,
-						detail: responsejson,
-					};
-				}
-			});
+			const responsetext= await response.text();
+			let responsejson = responsetext;
+			try {
+				responsejson = JSON.parse(responsetext);
+			} finally {
+				return {
+					error: true,
+					status: response.status,
+					text: responsetext,
+					url: response.url,
+					detail: responsejson,
+				};
+			}
 		}
 		return response.json();
-	});
+	}
+	
+	return await result;
 };
 
 export default GetRequest;
