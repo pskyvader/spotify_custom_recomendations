0466a4c1d2a8f3cf16ba9024e71be4f7197e4a44
Pablo <pskyvader@hotmail.com>
2022-01-17 01:44:18 -0600

    --- Auto-Git Commit ---

--- server/api/logincookie.js
+++ server/api/logincookie.js
@@ -1,11 +1,45 @@
 const { User } = require("../database/connection");
 
+const refreshcookie = async (req, res, currentUser) => {
+	const refresh_token = currentUser.refresh_token;
+	const requestOptions = {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded",
+			Authorization:
+				"Basic " +
+				Buffer.from(
+					credentials.client_id + ":" + credentials.client_secret
+				).toString("base64"),
+		},
+		body: new URLSearchParams({
+			grant_type: "refresh_token",
+			refresh_token: refresh_token,
+		}).toString(),
+	};
+	const response = await request(
+		req,
+		"https://accounts.spotify.com/api/token",
+		"POST",
+		null,
+		requestOptions
+	);
+	if (!response.error) {
+		console.log(response);
+		// const access_token = response.access_token || null;
+		// const refresh_token = response.refresh_token || null;
+		// const expires_in = response.expires_in || null;
 
-const refreshcookie= async(req, res,currentUser) => {
+		// req.session.loggedin = true;
+		// req.session.access_token = access_token;
+		// req.session.refresh_token = refresh_token;
+		// req.session.expiration = Date.now() + expires_in * 1000;
 
-}
-
-
+		res.redirect("/#loggedin=true");
+		return;
+	}
+	res.redirect("/#error=login_error");
+};
 
 const logincookie = async (req, res) => {
 	let result = { error: null };
@@ -29,13 +63,14 @@ const logincookie = async (req, res) => {
 			req.session.access_token = meProfileResult.access_token;
 			req.session.refresh_token = meProfileResult.refresh_token;
 			req.session.expiration = meProfileResult.expiration;
-			meProfileResult.loggedin=true;
+			meProfileResult.loggedin = true;
 			result = meProfileResult;
-			res.json(result);		
+			res.json(result);
 			return;
 		}
-		await refreshcookie(req,res,currentUser);
+		return refreshcookie(req, res, currentUser);
 	}
+	console.log(currentUser)
 
 	res.json(result);
 };
