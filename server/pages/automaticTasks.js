3129aabec27a864f6b809ece075eba0807195df9
Pablo <pskyvader@hotmail.com>
2022-03-28 01:49:47 -0500

    a

--- server/pages/automaticTasks.js
+++ server/pages/automaticTasks.js
@@ -59,9 +59,9 @@ const automaticTasks = async (req, res) => {
 				`user ${
 					user.id
 				} should be able to process requests, date: ${new Date(
-					Date.now() - 600000
+					Date.now() - 1200000
 				).toString()}, expiration:${user.expiration}, Minus: ${
-					Date.now() - 600000 - user.expiration
+					Date.now() - 1200000 - user.expiration
 				}, Minus:${user.expiration < Date.now() - 1200000},is date:${
 					user.expiration instanceof Date
 				} `
