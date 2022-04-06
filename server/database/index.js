c14b01d921237d45b24ac6fe72c66db3897f4ff1
Pablo <pskyvader@hotmail.com>
2022-04-06 17:05:45 -0500

    --- Auto-Git Commit ---

--- server/database/index.js
+++ server/database/index.js
@@ -55,7 +55,7 @@ const connection = async () => {
 	try {
 		await sequelize.authenticate();
 		console.log("Connection has been established successfully.");
-		// sequelize.sync({ force: true});
+		sequelize.sync({ force: true});
 		// sequelize.sync({ alter: true });
 		// sequelize.sync();
 	} catch (error) {
