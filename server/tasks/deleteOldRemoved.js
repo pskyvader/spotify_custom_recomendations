479fac718cd36f2b078d4e4591e51e4c1005952f
Pablo <pskyvader@hotmail.com>
2022-07-06 18:33:30 -0500

    a

--- server/tasks/deleteOldRemoved.js
+++ server/tasks/deleteOldRemoved.js
@@ -1,11 +1,13 @@
 const { Op } = require("sequelize");
 const { UserSong } = require("../database");
+//week in ms
+const week = 604800000;
 const deleteOldRemoved = async () => {
 	return UserSong.destroy({
 		where: {
 			[Op.and]: [
 				{ removed: true },
-				{ song_removed: { [Op.lt]: Date.now() - 604800000 } },
+				{ song_removed: { [Op.lt]: Date.now() - 2 * week } },
 			],
 		},
 		raw: true,
