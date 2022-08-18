5e6ed7b3d68073312ada016986026441b5dedf1f
Pablo <pskyvader@hotmail.com>
2022-08-18 00:04:20 -0500

    a

--- client/src/components/SongList.jsx
+++ client/src/components/SongList.jsx
@@ -43,7 +43,7 @@ const SongList = ({ data, title = "title" }) => {
 		<DataGrid
 			// autoPageSize={true}
 			// hideFooter
-			pageSize={1000}
+			pageSize={100}
 			rowsPerPageOptions={[10,25,50,100,200,1000]}
 			disableSelectionOnClick
 			components={{
