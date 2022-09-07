2c6fb1a85e2988bbded3c516b3280d9f74b13978
Pablo <pskyvader@hotmail.com>
2022-09-07 00:52:07 -0500

    a

--- server/tasks/deleteGarbage.js
+++ server/tasks/deleteGarbage.js
@@ -1,20 +1,53 @@
 const { Op } = require("sequelize");
-const { UserSong } = require("../database");
+const { PlaylistSong, UserSongHistory, Song } = require("../database");
 //week in ms
 const week = 604800000;
 const deleteGarbage = async () => {
-	return UserSong.destroy({
+	const destroyedPlaylistSong = await PlaylistSong.destroy({
 		where: {
-			[Op.and]: [
-				{ removed: true },
-				{ song_removed: { [Op.lt]: Date.now() - 2 * week } },
-			],
+			active: false,
+			removed_date: {
+				[Op.lte]: Date.now() - 2 * week,
+			},
 		},
-		raw: true,
-		nest: true,
-	}).catch((err) => {
-		return { error: err.message };
+	}).catch((err) => ({ error: err.message }));
+	if (destroyedPlaylistSong.error) {
+		return destroyedPlaylistSong;
+	}
+	const destroyedUserSong = await UserSongHistory.destroy({
+		where: {
+			played_date: {
+				[Op.lte]: Date.now() - 6 * week,
+			},
+		},
+	}).catch((err) => ({ error: err.message }));
+	if (destroyedUserSong.error) {
+		return destroyedUserSong;
+	}
+
+	const songList = await Song.findAll({
+		where: {
+			last_time_used: {
+				[Op.lte]: Date.now() - 1 * week,
+			},
+		},
+		include: [User, Playlist],
 	});
+
+	const removeSongTasks = songList.map((song) => {
+		if (song.User.length === 0 && song.Playlist.length === 0) {
+			return song.destroy().catch((err) => ({ error: err.message }));
+		}
+		return null;
+	});
+	return Promise.all(removeSongTasks).then(
+		(success) => {
+			console.log(success.length + " removed garbage songs");
+		},
+		(error) => {
+			console.log(error + "remove garbage songs error");
+		}
+	);
 };
 
 module.exports = { deleteGarbage };
