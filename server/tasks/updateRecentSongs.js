81f18797301ec53cca6634738862847a8794d7d2
Pablo <pskyvader@hotmail.com>
2022-10-05 04:35:12 -0500

    a

--- server/tasks/updateRecentSongs.js
+++ server/tasks/updateRecentSongs.js
@@ -8,7 +8,15 @@ const updateRecentSongs = async (user) => {
 	const userSongHistory = await user.getUserSongHistories();
 	const songsToAdd = recentSongsAPI.filter((song) => {
 		song.played_date = new Date(song.played_date).getTime();
+		console.log(song.name, song.played_date);
 		const found = userSongHistory.find((history) => {
+			if (
+				history.SongId === song.id &&
+				history.played_date.getTime() === song.played_date
+			) {
+				console.log(history.played_date.getTime());
+			}
+
 			return (
 				history.SongId === song.id &&
 				history.played_date.getTime() === song.played_date
@@ -35,9 +43,14 @@ const updateRecentSongs = async (user) => {
 			return createUserSong(user, song, songtoadd.played_date);
 		});
 	});
-	return Promise.all(addTasks).then(() => {
-		return { error: false, message: "Songs updated" };
-	});
+	return Promise.all(addTasks)
+		.then((results) => {
+			console.log("UPDATED SONGS", results);
+			return { error: false, message: `${results.length} Songs updated` };
+		})
+		.catch((err) => {
+			return { error: true, message: err.message };
+		});
 };
 
 module.exports = { updateRecentSongs };
