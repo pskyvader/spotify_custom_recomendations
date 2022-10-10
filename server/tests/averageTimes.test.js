1a3e7e121bf00eebf32fb598b0ae97209bbf6210
Pablo <pskyvader@hotmail.com>
2022-10-09 19:55:41 -0500

    --- Auto Git Commit ---

--- server/tests/averageTimes.test.js
+++ server/tests/averageTimes.test.js
@@ -2,13 +2,17 @@ require("dotenv").config();
 const { updateAverageTimes } = require("../tasks");
 const { User } = require("../database");
 
-const averageTimesTest = () => {
+test("Console render without errors", () => {
 	User.findOne()
 		.then((user) => updateAverageTimes(user))
 		.then((response) => {
 			console.log(response);
+			return response.error;
+		})
+		.finally((r) => {
+			expect(r).toBeFalsy();
 		});
-};
+});
 
 if (require.main === module) {
 	averageTimesTest();
