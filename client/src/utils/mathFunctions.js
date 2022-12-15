2e9fe4046a404aba567be4456a47c4ece0ced0e7
Pablo <pskyvader@hotmail.com>
2022-12-15 00:16:08 -0600

    --- Auto Git Commit ---

--- client/src/utils/mathFunctions.js
+++ client/src/utils/mathFunctions.js
@@ -34,4 +34,28 @@ const stdDeviation = (arr, usePopulation = false) => {
 	return Math.sqrt(total / (arr.length - (usePopulation ? 0 : 1)));
 };
 
-export { sum, average, stdDeviation };
+const filterOutliers = (someArray) => {
+	if (someArray.length < 4) return someArray;
+
+	let values, q1, q3, iqr, maxValue, minValue;
+
+	values = someArray.slice().sort((a, b) => a - b); //copy array fast and sort
+	const len = values.length / 4;
+
+	if (len % 1 === 0) {
+		//find quartiles
+		q1 = (1 / 2) * (values[len] + values[len + 1]);
+		q3 = (1 / 2) * (values[len * 3 - 1] + values[len * 3]);
+	} else {
+		q1 = values[Math.floor(len + 1)];
+		q3 = values[Math.ceil(len * 3)];
+	}
+
+	iqr = q3 - q1;
+	maxValue = q3 + iqr * 1.5;
+	minValue = q1 - iqr * 1.5;
+
+	return values.filter((x) => x >= minValue && x <= maxValue);
+};
+
+export { sum, average, stdDeviation, filterOutliers };
