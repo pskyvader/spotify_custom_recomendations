4791ffb1d598dd4796fe36a6600dd5d70ea51fea
Pablo <pskyvader@hotmail.com>
2023-01-12 00:55:00 -0600

    a

--- server/api/user/validateUserLogin.js
+++ server/api/user/validateUserLogin.js
@@ -2,19 +2,19 @@ const { createUser, getUser } = require("../../model");
 const { refreshCookie, getUser: getUserAPI } = require("../../spotifyapi/user");
 
 const validateUserLogin = async (loginData) => {
-	console.log("User validation");
+	// console.log("User validation");
 	const response = { error: true, message: "" };
 	let user = await getUser(loginData);
-	console.log("Getting user from database using login data", user);
+	// console.log("Getting user from database using login data", user);
 	if (user !== null) {
-		console.log("User not null");
-		if (user.error || Date.now() > new Date(user.expiration)) {
-			console.log("User found or error", user);
+		// console.log("User not null");
+		if (user.error || new Date(user.expiration) > Date.now()) {
+			// console.log("User found or error", user);
 			return user;
 		}
-		console.log("User expired", user, "login data", loginData);
+		// console.log("User expired", user, "login data", loginData);
 		loginData = { ...user, ...loginData };
-		console.log("Merged Data", loginData);
+		// console.log("Merged Data", loginData);
 	}
 
 	if (
@@ -23,39 +23,39 @@ const validateUserLogin = async (loginData) => {
 		!loginData.access_token ||
 		!loginData.expiration
 	) {
-		console.log("Invalid user Data", loginData);
+		// console.log("Invalid user Data", loginData);
 		response.message = "No valid user data found";
 		return response;
 	}
 
-	console.log("Refreshing cookie", loginData);
-	// if (Date.now() < new Date(loginData.expiration)) {
+	// console.log("Refreshing cookie", loginData);
+	// if (  new Date(loginData.expiration)< Date.now()) {
 	loginData = await refreshCookie(loginData);
 
-	console.log("After refresh", loginData);
+	// console.log("After refresh", loginData);
 	if (loginData.error) {
-		console.log("error on refresh", loginData);
+		// console.log("error on refresh", loginData);
 		return loginData;
 	}
 	// }
 
 	const formattedUser = await getUserAPI(loginData.access_token, loginData);
 	if (formattedUser.error) {
-		console.log("error on get user from api", formattedUser);
+		// console.log("error on get user from api", formattedUser);
 		return formattedUser;
 	}
 	const thisUser = await getUser(formattedUser);
-	console.log("Getting user from database using api data", thisUser);
+	// console.log("Getting user from database using api data", thisUser);
 	if (thisUser === null) {
-		console.log("Creating user in Database", formattedUser);
+		// console.log("Creating user in Database", formattedUser);
 		return createUser(formattedUser);
 	}
 
 	if (thisUser.error) {
-		console.log("Error getting user");
+		// console.log("Error getting user");
 		return thisUser;
 	}
-	console.log("Updating user in Database", formattedUser);
+	// console.log("Updating user in Database", formattedUser);
 	return thisUser.update(formattedUser);
 };
 
