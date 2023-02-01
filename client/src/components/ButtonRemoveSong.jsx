b5a49865ec2d19e8f899d22447c162037d2f0381
Pablo <pskyvader@hotmail.com>
2023-01-31 19:53:32 -0600

    a

--- client/src/components/ButtonRemoveSong.jsx
+++ client/src/components/ButtonRemoveSong.jsx
@@ -1,10 +1,9 @@
 import { Button } from "@mui/material";
 import { Playlist } from "../API";
-import { useContext, useState } from "react";
+import { useContext, useState, useCallback } from "react";
 import { PlaylistContext } from "../context/PlaylistContextProvider";
 
-const ButtonRemoveSong = ({ PlaylistId, uri }) => {
-	const [disabled, setDisabled] = useState(false);
+const PostProcessPlaylist = (response, PlaylistId) => {
 	const {
 		playlistTracks,
 		setPlaylistTracks,
@@ -13,57 +12,60 @@ const ButtonRemoveSong = ({ PlaylistId, uri }) => {
 		playlistDeletedSongs,
 		setPlaylistDeletedSongs,
 	} = useContext(PlaylistContext);
+
+	playlistTracks[PlaylistId] = playlistTracks[PlaylistId] || [];
+	playlistDeleteTracks[PlaylistId] = playlistDeleteTracks[PlaylistId] || [];
+	playlistDeletedSongs[PlaylistId] = playlistDeletedSongs[PlaylistId] || [];
+
+	const playlistFiltered = playlistTracks[PlaylistId].filter((song) => {
+		return response.id !== song.id;
+	});
+	if (playlistTracks[PlaylistId].length !== playlistFiltered.length) {
+		playlistTracks[PlaylistId] = playlistFiltered;
+		setPlaylistTracks({ ...playlistTracks });
+	}
+
+	const deleterecommendedfiltered = playlistDeleteTracks[PlaylistId].filter(
+		(song) => {
+			return response.id !== song.id;
+		}
+	);
+	if (
+		playlistDeleteTracks[PlaylistId].length !==
+		deleterecommendedfiltered.length
+	) {
+		playlistDeleteTracks[PlaylistId] = deleterecommendedfiltered;
+		setPlaylistDeleteTracks({
+			...playlistDeleteTracks,
+		});
+	}
+
+	playlistDeletedSongs[PlaylistId] = [
+		response.song,
+		...playlistDeletedSongs[PlaylistId],
+	];
+	setPlaylistDeletedSongs({
+		...playlistDeletedSongs,
+	});
+};
+
+const ButtonRemoveSong = ({ PlaylistId, uri }) => {
+	const [disabled, setDisabled] = useState(false);
+
+	const deleteButtonCallback = useCallback(() => {
+		setDisabled(true);
+		Playlist.DeleteSong(PlaylistId, uri).then((response) => {
+			setDisabled(false);
+			if (response.error) {
+				console.log(response);
+				return;
+			}
+			PostProcessPlaylist(response, PlaylistId);
+		});
+	}, [PlaylistId, uri]);
+
 	return (
-		<Button
-			disabled={disabled}
-			onClick={() => {
-				setDisabled(true);
-				Playlist.DeleteSong(PlaylistId, uri).then((response) => {
-					setDisabled(false);
-					if (response.error) {
-						console.log(response);
-						return;
-					}
-
-					const playlistFiltered = playlistTracks[PlaylistId].filter(
-						(song) => {
-							return response.id !== song.id;
-						}
-					);
-					if (
-						playlistTracks[PlaylistId].length !==
-						playlistFiltered.length
-					) {
-						playlistTracks[PlaylistId] = playlistFiltered;
-						setPlaylistTracks({ ...playlistTracks });
-					}
-
-					const deleterecommendedfiltered = playlistDeleteTracks[
-						PlaylistId
-					].filter((song) => {
-						return response.id !== song.id;
-					});
-					if (
-						playlistDeleteTracks[PlaylistId].length !==
-						deleterecommendedfiltered.length
-					) {
-						playlistDeleteTracks[PlaylistId] =
-							deleterecommendedfiltered;
-						setPlaylistDeleteTracks({
-							...playlistDeleteTracks,
-						});
-					}
-
-					playlistDeletedSongs[PlaylistId] = [
-						response.song,
-						...playlistDeletedSongs[PlaylistId],
-					];
-					setPlaylistDeletedSongs({
-						...playlistDeletedSongs,
-					});
-				});
-			}}
-		>
+		<Button disabled={disabled} onClick={deleteButtonCallback}>
 			Remove
 		</Button>
 	);
