88b0150a3208d97b25184fe1f0ee011345eeec97
Pablo <pskyvader@hotmail.com>
2023-02-22 23:33:40 -0600

    --- Auto Git Commit ---

--- server/api/song/getRecommendedSongs.js
+++ server/api/song/getRecommendedSongs.js
@@ -8,72 +8,61 @@ const {
 
 //day in ms
 const day = 86400000;
-const getRecommendedSongs = async (user, playlist, minTime = null) => {
-	const minTimeInPlaylist = day * (minTime && minTime > 5 ? minTime : 5);
+
+const getRecommendedSongs = async (user, playlist, minDays = null) => {
+	const minDaysInPlaylist = day * (minDays && minDays > 5 ? minDays : 5);
 
 	let fullPlaylist = await getPlaylistSongs(playlist);
 
 	let currentPlaylist = await getPlaylistSongs(
 		playlist,
-		Date.now() - 2 * minTimeInPlaylist
-	); // filter by added 2 minTimeInPlaylist ago or earlier
-	if (currentPlaylist.error) {
-		return currentPlaylist;
-	}
+		Date.now() - 2 * minDaysInPlaylist
+	); // filter by added 2 minDaysInPlaylist ago or earlier
+	if (currentPlaylist.error) return currentPlaylist;
 
-	// Recent: get playlist songs ids and played more recently than 2 minTimeInPlaylist ago
 	const RecentSongs = await getRecentlyPlayedSongs(
 		user,
-		Date.now() - 2 * minTimeInPlaylist
+		Date.now() - 2 * minDaysInPlaylist
 	);
-	if (RecentSongs.error) {
-		return RecentSongs;
-	}
-	const RecentSongsIds = RecentSongs.map((currentSong) => {
-		return currentSong.Song.id;
-	});
+	if (RecentSongs.error) return RecentSongs;
+
+	const RecentSongsIds = RecentSongs.map(
+		(currentSong) => currentSong.Song.id
+	);
 
 	const topSongs = await getTopSongs(user.access_token);
-	if (topSongs.error) {
-		return topSongs;
-	}
+	if (topSongs.error) return topSongs;
 
 	const topSongsIds = topSongs.map((song) => song.id);
 
-	//Recommended songs: get playlist songs in recent playlist that are in current playlist, or in top songs
 	let recommendedSongs = [
-		...currentPlaylist.filter((currentSong) => {
-			return topSongsIds.includes(currentSong.id);
-		}),
-		...RecentSongsIds.filter((currentSong) => {
-			return fullPlaylist.includes(currentSong.id);
-		}).reverse(),
+		...currentPlaylist.filter((currentSong) =>
+			topSongsIds.includes(currentSong.id)
+		),
+		...RecentSongsIds.filter((currentSong) =>
+			fullPlaylist.includes(currentSong)
+		).reverse(),
 	];
 
 	if (recommendedSongs.length === 0) {
-		console.log(
-			`No recommended songs for playlist ${playlist.id}, getting top songs`
-		);
 		recommendedSongs = topSongs;
 	}
 
 	const recommendedTracks = await getRecommendedSongsAPI(
 		user.access_token,
 		recommendedSongs,
-		fullPlaylist.length
+		fullPlaylist.length,
+		country.country
 	);
-	if (recommendedTracks.error) {
-		return recommendedTracks;
-	}
+	if (recommendedTracks.error) return recommendedTracks;
 
 	const removedSongs = await getDeletedSongs(playlist);
-	//remove songs recently removed and songs already in playlist
-	return recommendedTracks.filter((currentSong) => {
-		return (
+
+	return recommendedTracks.filter(
+		(currentSong) =>
 			!removedSongs.find((song) => song.id === currentSong.id) &&
 			!fullPlaylist.find((song) => song.id === currentSong.id)
-		);
-	});
+	);
 };
 
 module.exports = {
