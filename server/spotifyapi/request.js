12ffaf521ddd230dfac995a1bfba9f48705e517a
pskyvader <pskyvader@hotmail.com>
2025-01-12 12:44:15 -0600

    Update request.js

--- server/spotifyapi/request.js
+++ server/spotifyapi/request.js
@@ -61,6 +61,7 @@ const request = async (
 						},
 					};
 					try {
+						finalresponse.message.requestOptions = JSON.parse(finalresponse.message.requestOptions);
 						finalresponse.message.detail = JSON.parse(responsetext);
 						finalresponse.message = JSON.parse(
 							finalresponse.message
