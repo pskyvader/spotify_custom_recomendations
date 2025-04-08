57109d705f9237ab64960cfc6250a186f13ed5df
pskyvader <pskyvader@hotmail.com>
2025-04-08 12:30:48 -0500

    Update getRecommendedSongs.js
    Fix recommendations (in progress)

--- server/spotifyapi/song/getRecommendedSongs.js
+++ server/spotifyapi/song/getRecommendedSongs.js
@@ -39,16 +39,16 @@ const getRandomSongURL = (songlist) => {
 	const half_songlist = Math.floor((songlist.length - 1) / 2);
 	const index = Math.floor(Math.random() * half_songlist);
 	const randomSong = songlist[index];
-	const url = "https://api.spotify.com/v1/recommendations";
-	const urlOptions = `?seed_tracks=${randomSong.id}&limit=10`;
+	const url = "https://api.spotify.com/v1/search";
+	const urlOptions = `?q=album:${randomSong.album}%20artist:${randomSong.artist}&limit=10&type=track`;
 	return url + urlOptions;
 };
 const getRandomArtistURL = (songlist) => {
 	const half_songlist = Math.floor((songlist.length - 1) / 2);
 	const index = Math.floor(Math.random() * half_songlist);
 	const randomSong = songlist[index];
-	const url = "https://api.spotify.com/v1/recommendations";
-	const urlOptions = `?seed_artists=${randomSong.idartist}&limit=10`;
+	const url = "https://api.spotify.com/v1/search";
+	const urlOptions = `?q=artist:${randomSong.artist}&limit=10&type=track`;
 	return url + urlOptions;
 };
 
