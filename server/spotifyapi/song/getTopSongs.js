21100b7824f006cd65d2fc32777d200a7cfd7e17
pskyvader <pskyvader@hotmail.com>
2025-04-09 19:31:14 -0500

    Update getTopSongs.js

--- server/spotifyapi/song/getTopSongs.js
+++ server/spotifyapi/song/getTopSongs.js
@@ -1,10 +1,11 @@
 const { request } = require("../");
 const { formatSongGroup } = require("./formatSong");
+const limit=50;
 
 const getTopSongs = async (access_token) => {
-	let url = "https://api.spotify.com/v1/me/top/tracks?limit=50"; //&time_range=long_term
+	let url = `https://api.spotify.com/v1/me/top/tracks?limit=${limit}`; //&time_range=long_term
 	let items = [];
-	while (url) {
+	while (url && items.length<limit) {
 		const response = await request(access_token, url);
 		if (response.error) {
 			console.log("Get top songs from API error", response);
