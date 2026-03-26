223b56bfae890658dfd4f1b87bf85cd455b5395a
pskyvader <pskyvader@hotmail.com>
2026-03-25 22:43:40 -0500

    Update getSong.js

--- server/api/song/getSong.js
+++ server/api/song/getSong.js
@@ -1,14 +1,22 @@
 const { getSong: getSongDB, createSong } = require("../../model");
 const { getSong: getSongAPI } = require("../../spotifyapi/song");
+const {log}=require("../../utils/logger");
+
 
 const getSong = (access_token, songId, songData = null) => {
+	log("api/song/get song");
 	return getSongDB(songId)
 		.then((currentSong) => {
+			log(`current song: ${currentSong}`);
 			if (currentSong === null) {
+				log("current song null");
 				if (songData !== null) {
+					log("song data not null, creating song in db");
 					return createSong(songData);
 				}
+				log("get song from api");
 				return getSongAPI(access_token, songId).then((songFromAPI) => {
+					log(`song from api: ${songFromAPI}`);
 					if (songFromAPI.error) {
 						return songFromAPI;
 					}
@@ -16,8 +24,10 @@ const getSong = (access_token, songId, songData = null) => {
 				});
 			}
 			if (currentSong.error) {
+				log ("current song error");
 				return currentSong;
 			}
+			log("update song data");
 			return currentSong.update(songData);
 		})
 		.catch((err) => {
