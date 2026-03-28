718106c8c3536a3ecb81bb2860b7281c1a9d26ec
pskyvader <pskyvader@hotmail.com>
2026-03-28 11:53:39 -0500

    Update getSong.js

--- server/api/song/getSong.js
+++ server/api/song/getSong.js
@@ -4,19 +4,19 @@ const {log}=require("../../utils/logger");
 
 
 const getSong = (access_token, songId, songData = null) => {
-	log("api/song/get song");
+	//log("api/song/get song");
 	return getSongDB(songId)
 		.then((currentSong) => {
-			log(`current song: ${currentSong}`);
+			//log(`current song: ${currentSong}`);
 			if (currentSong === null) {
-				log("current song null");
+				//log("current song null");
 				if (songData !== null) {
-					log("song data not null, creating song in db");
+					//log("song data not null, creating song in db");
 					return createSong(songData);
 				}
-				log("get song from api");
+				//log("get song from api");
 				return getSongAPI(access_token, songId).then((songFromAPI) => {
-					log(`song from api: ${songFromAPI}`);
+					//log(`song from api: ${songFromAPI}`);
 					if (songFromAPI.error) {
 						return songFromAPI;
 					}
@@ -24,10 +24,10 @@ const getSong = (access_token, songId, songData = null) => {
 				});
 			}
 			if (currentSong.error) {
-				log ("current song error");
+				//log ("current song error");
 				return currentSong;
 			}
-			log("update song data");
+			//log("update song data");
 			return currentSong.update(songData);
 		})
 		.catch((err) => {
