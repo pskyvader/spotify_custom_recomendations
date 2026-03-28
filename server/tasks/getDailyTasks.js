229a2adce6583e3299e12fb161e1936580f58f0c
pskyvader <pskyvader@hotmail.com>
2026-03-28 18:10:49 -0500

    Refactor getDailyTasks to use async/await
    Refactor getDailyTasks to use async/await for better readability and error handling.

--- server/tasks/getDailyTasks.js
+++ server/tasks/getDailyTasks.js
@@ -3,49 +3,76 @@ const { removeFromPlaylist } = require("./removeFromPlaylist");
 const { addToPlaylist } = require("./addToPlaylist");
 const { deleteGarbage } = require("./deleteGarbage");
 const { removeRepeatedSongs } = require("./removeRepeatedSongs");
-const { log, error} = require("../utils/logger");
-
+const { log, error } = require("../utils/logger");
 
 const getDailyTasks = (userList) => {
 	const songsToModify = 1 + Math.floor(Math.random() * 4);
+
 	log("beginning daily tasks");
+
 	const taskList = userList.map((user) => {
-		log("remove repeated songs");
-		return removeRepeatedSongs(user)
-			.then((responseRepeated) =>{
+		// 👇 RETURN A FUNCTION (NOT A PROMISE)
+		return async () => {
+			try {
+				log("remove repeated songs");
+
+				const responseRepeated = await removeRepeatedSongs(user);
+
 				log("update times");
-				return updateAverageTimes(user, responseRepeated);
-			}
-			)
-			.then((averageResponse) =>{
+
+				const averageResponse = await updateAverageTimes(
+					user,
+					responseRepeated
+				);
+
 				log("remove from playlist");
-				return removeFromPlaylist(user, songsToModify, averageResponse);
-			}
-			)
-			.then((removeResponse) => {
-				log ("add to playlist");
-				return addToPlaylist(
+
+				const removeResponse = await removeFromPlaylist(
 					user,
 					songsToModify,
-					removeResponse.removedTotal, // removedTotal []: avoid adding too many songs to the playlist if it's already over the limit
+					averageResponse
+				);
+
+				log("add to playlist");
+
+				const addResponse = await addToPlaylist(
+					user,
+					songsToModify,
+					removeResponse.removedTotal,
 					removeResponse
 				);
-			})
-			.then((addResponse) => {
-				log("save reaponse");
+
+				log("save response");
+
 				user.set({ last_modified_daily: Date.now() });
-				return user.save().then(() => {
-					addResponse.message.push(
-						`last daily updated for user ${user.name} saved`
-					);
-					return addResponse;
+
+				await user.save();
+
+				addResponse.message.push(
+					`last daily updated for user ${user.name} saved`
+				);
+
+				return addResponse;
+			} catch (err) {
+				// you said catch is handled elsewhere, but we still need a return shape
+				error("Daily task failed", {
+					userId: user.id,
+					error: err?.message,
 				});
-			});
+
+				return { error: true };
+			}
+		};
 	});
+
 	if (userList.length > 0) {
-		log("delete garbage");
-		taskList.push(deleteGarbage());
+		taskList.push(async () => {
+			log("delete garbage");
+			return await deleteGarbage();
+		});
 	}
+
 	return taskList;
 };
+
 module.exports = { getDailyTasks };
