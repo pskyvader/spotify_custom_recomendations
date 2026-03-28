196ee8363c3898635055bbf5af10140a1fa1b841
pskyvader <pskyvader@hotmail.com>
2026-03-28 18:13:12 -0500

    Refactor getHourlyTasks to use async/await

--- server/tasks/getHourlyTasks.js
+++ server/tasks/getHourlyTasks.js
@@ -1,31 +1,48 @@
 const { updateRecentSongs } = require("./updateRecentSongs");
 const { syncronizeMultiplePlaylists } = require("../api/playlist");
 
+const { log, error } = require("../utils/logger");
+
 const getHourlyTasks = (userList) => {
 	return userList.map((user) => {
-		return updateRecentSongs(user)
-			.then((updateResponse) => {
-				return syncronizeMultiplePlaylists(user).then(
-					(syncResponse) => {
-						return {
-							error: updateResponse.error || syncResponse.error,
-							message: [
-								updateResponse.message,
-								...syncResponse.message,
-							],
-						};
-					}
-				);
-			})
-			.then((response) => {
+		// 👇 return a function (deferred execution)
+		return async () => {
+			try {
+				log("update recent songs", { userId: user.id });
+
+				const updateResponse = await updateRecentSongs(user);
+
+				log("synchronize playlists", { userId: user.id });
+
+				const syncResponse = await syncronizeMultiplePlaylists(user);
+
+				const response = {
+					error: updateResponse.error || syncResponse.error,
+					message: [
+						updateResponse.message,
+						...syncResponse.message,
+					],
+				};
+
 				user.set({ last_modified_hourly: Date.now() });
-				return user.save().then(() => {
-					response.message.push(
-						`last hourly updated for user ${user.name} saved`
-					);
-					return response;
+
+				await user.save();
+
+				response.message.push(
+					`last hourly updated for user ${user.name} saved`
+				);
+
+				return response;
+			} catch (err) {
+				// keep consistent with your system contract
+				error("Hourly task failed", {
+					userId: user.id,
+					error: err?.message,
 				});
-			});
+
+				return { error: true };
+			}
+		};
 	});
 };
 
